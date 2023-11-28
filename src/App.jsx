dcdd10761c62baa2cdc999be1a151989dcc82aea
Raina Bethea <rainabethea@gmail.com>
2023-11-28 17:41:39 -0500

    puppy data and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
-  return (
+return (
     <>
-     
+     <h1>Puppy Pal Workshop</h1>
+     <div>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
+     </div>
     </>
   )
 }
